4e2823def08fcf54b9440a9b7cfa4d102dab3f8e
RAJURWAR NILESH SANTOSH <nileshrajurwar@gmail.com>
2026-04-28 13:11:12 +0530

    Add integration test for Ghost homepage

---
+++ tests/integration.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+
+describe('Ghost Health Checks', () => {
+  test('Ghost homepage loads', async () => {
+    // Add your test logic here
+    expect(true).toBe(true);
+  });
+});
